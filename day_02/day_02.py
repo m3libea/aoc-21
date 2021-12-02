acbf2707ec25c3925ba64278be5549ac9d412b75
Rocio Romero <rociorommor@gmail.com>
2021-12-02 00:09:26 -0800

    Refactor

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -4,54 +4,33 @@ import sys
 sys.path.insert(0, '../')
 from utils import advent
 
-def parse_input(input):
-    with open(input, 'r') as file:
-        lines = [parse_line(line) for line in file.readlines()]
-    return lines
-
 def parse_line(line):
-    splitted = line.split(" ")
-    return [splitted[0], int(splitted[1])]
+    split = line.split(" ")
+    return [split[0], int(split[1])]
 
 def get_position(directions):
     horizontal = 0
     depth = 0
-    for move in directions:
-        if move[0] == "forward":
-            horizontal += move[1]
-        else:
-            direction = 1 if move[0] == "down" else -1
-            depth += direction * move[1] 
-
-    return [horizontal, depth]
-
-def get_position_with_aim(directions):
-    horizontal = 0
-    depth = 0
     aim = 0
-    for move in directions:
-        if move[0] == "forward":
-            horizontal += move[1]
-            depth += move[1] * aim
-        else:
-            direction = 1 if move[0] == "down" else -1
-            aim += direction * move[1] 
 
-    return [horizontal, depth]
+    for move in directions:
+        match move[0]:
+            case "forward":
+                horizontal += move[1]
+                depth += move[1] * aim 
+            case "down":
+                aim += move[1] 
+            case "up":
+                aim -= move[1]
+    return horizontal, depth, aim
 
 if __name__ == '__main__':
 
-    directions = parse_input("input")
-
-    position = get_position(directions)
-    total = position[0] * position[1]
+    directions = advent.parse_input("input", parse_line)
+    horizontal, depth, aim = get_position(directions)
 
     #Part 1: What do you get if you multiply your final horizontal position by your final depth?
-    advent.print_answer(1, total)
+    advent.print_answer(1, horizontal * aim)
 
     #Part 2: What do you get if you multiply your final horizontal position by your final depth? (with Aim)
-
-    position_two = get_position_with_aim(directions)
-    total = position_two[0] * position_two[1]
-
-    advent.print_answer(2, total)
+    advent.print_answer(2, horizontal * depth)
