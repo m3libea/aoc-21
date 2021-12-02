a9ffe3b68bb9d753b9dfd293012a69ba3d16a15c
Rocio Romero <rociorommor@gmail.com>
2021-12-02 00:12:17 -0800

    parse_lines moved to utils

--- utils/advent.py
+++ utils/advent.py
@@ -7,3 +7,9 @@ def read_numbers_lines(input):
     with open(input, 'r') as file:
         lines = [int(line) for line in file.readlines()]
     return lines
+
+#Parse input line by line using a given function for the line
+def parse_input(input, parse_line):
+    with open(input, 'r') as file:
+        lines = [parse_line(line) for line in file.readlines()]
+    return lines
