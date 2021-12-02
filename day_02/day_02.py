5fd98b40457b2c86be46b05b6eb9577fd2a70ee3
Rocio Romero <rociorommor@gmail.com>
2021-12-01 23:43:41 -0800

    part 2

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -25,13 +25,33 @@ def get_position(directions):
 
     return [horizontal, depth]
 
+def get_position_with_aim(directions):
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for move in directions:
+        if move[0] == "forward":
+            horizontal += move[1]
+            depth += move[1] * aim
+        else:
+            direction = 1 if move[0] == "down" else -1
+            aim += direction * move[1] 
+
+    return [horizontal, depth]
 
 if __name__ == '__main__':
 
     directions = parse_input("input")
-    position = get_position(directions)
 
+    position = get_position(directions)
     total = position[0] * position[1]
 
     #Part 1: What do you get if you multiply your final horizontal position by your final depth?
     advent.print_answer(1, total)
+
+    #Part 2: What do you get if you multiply your final horizontal position by your final depth? (with Aim)
+
+    position_two = get_position_with_aim(directions)
+    total = position_two[0] * position_two[1]
+
+    advent.print_answer(2, total)
