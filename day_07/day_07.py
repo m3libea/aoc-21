206c1ff33d283e74934b08fe4b01e2f843994d9b
Rocio Romero <rociorommor@gmail.com>
2021-12-06 22:33:26 -0800

    part 2

--- day_07/day_07.py
+++ day_07/day_07.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pathlib
 import sys
+
 sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent))
 from utils import advent
 
@@ -15,12 +16,25 @@ def get_cost(input):
 
     return total
 
+# Fuel cost: n * (n+1)/2
+def get_cost_part_two(input):
+    average = int(np.mean(input))
+   
+    total = 0
+    for num in input:
+        n = abs(num - average)
+        total += (n * (n + 1)) // 2
+    return total
+
 if __name__ == '__main__':
 
     example = "16,1,2,0,4,2,7,1,2,14"
-    positions = parse_line(example)
+    positions_example = parse_line(example)
 
     positions = advent.parse_input("input", parse_line)
 
     part_one = get_cost(positions[0])
     advent.print_answer(1, part_one)
+
+    part_two = get_cost_part_two(positions[0])
+    advent.print_answer(2, part_two)
